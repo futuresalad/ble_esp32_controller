ee13a59d0df5a56d6a474a28429714d8fbe176be
Ephraim Westenberger <ephraim.westenberger@gmail.com>
2023-11-24 23:48:08 +0100

    Implemented permission request prompt

--- app/src/main/java/com/example/ble_esp32_controller/BleDeviceScanActivity.java
+++ app/src/main/java/com/example/ble_esp32_controller/BleDeviceScanActivity.java
@@ -1,11 +1,9 @@
 package com.example.ble_esp32_controller;
-
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothManager;
 import android.bluetooth.le.BluetoothLeScanner;
@@ -26,19 +24,16 @@ public class BleDeviceScanActivity extends AppCompatActivity {
     private boolean scanning;
     private static final long SCAN_PERIOD = 10000;
     private final Handler handler = new Handler(Objects.requireNonNull(Looper.myLooper()));
-    private BluetoothManager bluetoothManager;
     private BluetoothAdapter bluetoothAdapter;
-    private BluetoothLeScanner bluetoothLeScanner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ble_device_scan);
 
-        bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
         bluetoothAdapter = (android.bluetooth.BluetoothAdapter) bluetoothManager.getAdapter();
-        bluetoothLeScanner = bluetoothAdapter.getBluetoothLeScanner();
-
+        BluetoothLeScanner bluetoothLeScanner = bluetoothAdapter.getBluetoothLeScanner();
         ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.BLUETOOTH_CONNECT);
 
     }
@@ -53,12 +48,10 @@ public class BleDeviceScanActivity extends AppCompatActivity {
 
     private void promptEnableBluetooth() {
         if (!bluetoothAdapter.isEnabled()) {
+
             Intent i = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
 
-            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.BLUETOOTH_CONNECT) == PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(this, "Permission to use Bluetooth is granted", Toast.LENGTH_LONG).show();
-
-            } else if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.BLUETOOTH_CONNECT)) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
 
                 AlertDialog.Builder builder = new AlertDialog.Builder(this);
                 builder.setMessage("Permission for Bluetooth required")
@@ -67,19 +60,23 @@ public class BleDeviceScanActivity extends AppCompatActivity {
                         .setPositiveButton("Aight", new DialogInterface.OnClickListener() {
                                     @Override
                                     public void onClick(DialogInterface dialog, int which) {
-                                        ActivityCompat.requestPermissions(BleDeviceScanActivity.this, new String[]{android.Manifest.permission.BLUETOOTH_CONNECT}, ENABLE_BLUETOOTH_REQUEST_CODE);
+                                        ActivityCompat.requestPermissions(BleDeviceScanActivity.this, new String[]{Manifest.permission.BLUETOOTH_CONNECT}, ENABLE_BLUETOOTH_REQUEST_CODE);
                                     }
                                 }
                         )
-                        .setNegativeButton("Cancel", ((dialog, which)->dialog.dismiss()));
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast toast = Toast.makeText(BleDeviceScanActivity.this, "Bro you NEED to give permission for Bluetooth man :(", Toast.LENGTH_LONG);
+                                toast.show();
+                            }
+                        });
                 builder.show();
             }
             else {
-                ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.BLUETOOTH_CONNECT}, ENABLE_BLUETOOTH_REQUEST_CODE);
+                startActivity(i);
             }
-            startActivity(i);
         }
-        return;
     }
 }
 
