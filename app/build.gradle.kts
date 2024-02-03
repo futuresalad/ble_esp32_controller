7bb42327d2c0f5a8f0d6d9c255bc9116cfd15a28
Ephraim Westenberger <76460220+futuresalad@users.noreply.github.com>
2024-02-03 21:21:40 +0100

    Update build.gradle.kts
    Added dependencies for database

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,4 +42,11 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
+    // Dependencies for room db
+    implementation("androidx.room:room-runtime:2.6.1")
+    annotationProcessor("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+
 }
